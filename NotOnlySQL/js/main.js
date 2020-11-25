2dbad145fe93a913232ac6aa45e02e1083646286
Maxim Glazkov <53126226+AllueXD@users.noreply.github.com>
2020-11-25 07:18:39 +0300

    123
    123

--- NotOnlySQL/js/main.js
+++
@@ -1,9 +0,0 @@
-
-function setIfFocused(x){
-  x.classList.remove('bg-dark');
-  x.classList.add('block2');
-}
-function setIfUnfocused(x){
-  x.classList.remove('block2');
-  x.classList.add('bg-dark');
-}
